c68cc2a07d2eebef3df2eea225afac686ea7b66e
dinhkaito <93849364+dinhkaito@users.noreply.github.com>
2021-11-07 09:40:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi crush của tớ",
+    introTitle: 'Crush yêu dấu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
