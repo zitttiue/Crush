6fc047c83cfd4945a5300daa3e7446cf493be48c
zitttiue <painsadno4@gmail.com>
2025-05-23 20:02:00 +0700

    Update zitttiue
    iu emmm vai

--- config.js
+++ config.js
@@ -1,20 +1,22 @@
 const CONFIG = {
-    titleWeb: "Gửi crush của tớ",
-    introTitle: 'Crush yêu dấu',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Gửi em người anh iuuu thương nhất",
+    introTitle: 'khvyyy yêu dấu của anhh',
+    introDesc: `Trái tim anh đã vốn đã nguội lạnh
+    mà sao khi thấy emmm nó lại đập nhanh bất thường  <33,
+    trong trái tim anhh là thứ zô cùng quý giá
+    mà sao
+    emmm lại nằm gọn trong trái tim anhh😋`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
+    title: 'Emmm gặp được anhh có phải là duyên số. Phải chăng emmm đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Thoát web là chấp nhận quen anhh rồi nhé!!!',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 8 tỉ người mà sao emmm lại lựa chọn iuuu anhh <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://facebook.com/dinhkaito' //link mess của các bạn. VD: /dinhkaito
+    mess: 'Anhh biết màaaaa 🥰. Yêu emmm nhiều nhiều lắm luôn rồi đó😘😘',
+    messDesc: 'Tối nay, emmm nhớ hunnn anhh một cái rồi mới được ngủ nhaaaaaa.',
+    btnAccept: 'Okiiiii lun ',
+    messLink: 'https://www.facebook.com/thieen.nhzan?mibextid=wwXIfr&mibextid=wwXIfr' //link mess của các bạn. VD: /dinhkaito
 }
+
